a4c0b7f8f3eab1a687a1f3497b4ee2e3114cdfb7
Selvaa Jayan <112411803+SelvaaIFETian@users.noreply.github.com>
2025-08-09 12:46:18 +0530

    Update superAdminRoutes.js

--- Backend/Routes/superAdminRoutes.js
+++ Backend/Routes/superAdminRoutes.js
@@ -2,9 +2,10 @@
 const express = require('express');
 const router = express.Router();
 const superAdminController = require('../Controllers/superAdminController');
+const userController=require('../Controllers/userController');
 
 router.post('/signup', superAdminController.signup);
 router.post('/login', superAdminController.login);
 router.post('/create-admin', superAdminController.createAdmin);
-
+router.get('/',  userController.getAllUsers); 
 module.exports = router;
