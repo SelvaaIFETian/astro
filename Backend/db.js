085beebb47fa36726bbd814b46a380cb76a86bdc
Selvaa Jayan <112411803+SelvaaIFETian@users.noreply.github.com>
2025-08-09 12:28:40 +0530

    Update db.js again

--- Backend/db.js
+++ Backend/db.js
@@ -1,4 +1,3 @@
-require('dotenv').config();
 const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize(
@@ -7,14 +6,10 @@ const sequelize = new Sequelize(
   process.env.DB_PASSWORD,
   {
     host: process.env.DB_HOST,
-    dialect: process.env.DB_DIALECT,
     port: process.env.DB_PORT,
-    logging: false
+    dialect: process.env.DB_DIALECT,
+    logging: false,
   }
 );
-
-sequelize.authenticate()
-  .then(() => console.log('✅ PostgreSQL connected'))
-  .catch(err => console.error('❌ Connection error:', err));
 
 module.exports = sequelize;
