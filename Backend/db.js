90d9c086eba5ec83ca8aeb9ee2d5f22bbfb1b8f7
Selvaa Jayan <112411803+SelvaaIFETian@users.noreply.github.com>
2025-08-09 11:55:09 +0530

    Update db.js

--- Backend/db.js
+++ Backend/db.js
@@ -1,11 +1,17 @@
 require('dotenv').config();
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
-  dialect: 'postgres',
-  protocol: 'postgres',
-  logging: false, // optional, remove logs
-});
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT,
+    port: process.env.DB_PORT,
+    logging: false
+  }
+);
 
 sequelize.authenticate()
   .then(() => console.log('✅ PostgreSQL connected'))
